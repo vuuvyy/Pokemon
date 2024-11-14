53397a0ebc50d9d22ff62b6e1ee146d4b0b18508
Sainoo <nvtrung19.work@gmail.com>
2024-11-14 10:55:10 +0700

    complete api detail

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+router.get('/userss', function(req, res, next) {
   res.send('respond with a resource');
 });
 
